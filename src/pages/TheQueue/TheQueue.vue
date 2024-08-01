631819752d8cad9d1d7dcbe1a0e609056910dc02
Palwan Ashirov <palwan.ashirov@gmail.com>
2024-08-02 04:07:12 +0500

    test: show telegram web app data

--- src/pages/TheQueue/TheQueue.vue
+++ src/pages/TheQueue/TheQueue.vue
@@ -1,7 +1,10 @@
 <template>
   <div>Queue Page</div>
+  <p v-text="telegramToken"></p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const telegramToken = computed(() => Telegram?.WebView.initParams.tgWebAppData || 'empty')
+</script>
 
 <style scoped></style>
